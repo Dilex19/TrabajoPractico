b6acb630747725349254288658b8f14703d5bc58
Dilex19 <diegoalexd04@gmail.com>
2024-10-12 03:41:43 -0300

    tortugas

---
+++ ProyectoLimpio/src/juego/Tortuga.java
@@ -0,0 +1,93 @@
+package juego;
+
+import java.awt.Image;
+
+import entorno.Entorno;
+
+public class Tortuga {
+	private double x,y;
+	private int velocidad;
+	private double ancho;
+	private double alto;
+	private double escala;
+	boolean direccion;
+	Entorno e;
+	Image Izq;
+	Image Der;
+	boolean apoyado;
+	boolean direccionAleatoria;
+	
+	public Tortuga(Entorno e) {
+		this.x = spawnRandom(e);
+		this.y = -20;
+		this.e = e;
+		this.direccion = false;
+		this.direccionAleatoria = false;
+		this.velocidad = 2;
+		this.escala =0.1;
+		this.Izq = entorno.Herramientas.cargarImagen("Rojo.png");
+		this.Der = entorno.Herramientas.cargarImagen("Rojo.png");
+		this.apoyado = false;
+		this.alto = this.Izq.getHeight(null) * this.escala;
+		this.ancho = this.Izq.getWidth(null) * this.escala;
+	}
+	
+	
+	public void gravedad() {
+		if(!apoyado) {
+			this.y +=2;
+		}
+	}
+	
+	//Dibujar Tortuga
+		public void dibujar() {
+			if(this.direccion == true) {
+				this.e.dibujarImagen(this.Izq, getX(), getY(), 0, escala);
+			} else {
+				this.e.dibujarImagen(this.Der, getX(), getY(), 0, escala);
+			}
+		}
+	
+	
+	
+	//Elige unas coordenadas aleatorioas din contar el "medio"
+	private int spawnRandom(Entorno e) {
+		int x = (int) (Math.random() * e.ancho());
+		while(x <450 && x >300) {
+			x = (int) (Math.random() * e.ancho());
+		}
+		return x;
+	}
+
+
+
+	public double getBorderSuperior() { // retorna el borde de la parte superior de la imagen
+		return this.y - this.alto/2;
+	}
+	public double getBorderInferior() {// retorna el borde de la parte inferior de la imagen
+		return this.y + this.alto/2;
+	}
+	public double getBorderIzquierdo() { // retorna el borde de la parte izquierda de la imagen
+		return this.x - this.ancho/2;
+	}
+	public double getBorderDerecho() { // retorna el borde de la parte izquierda de la imagen
+		return this.x + this.ancho/2;
+	}
+
+
+	public double getX() {
+		return x;
+}
+	public double getY() {
+		return y;
+	}
+
+
+	public void movimientoX() {
+		if (this.direccion) {
+			this.x +=velocidad;
+		} else {
+			this.x -=velocidad;
+		}
+	}
+}
